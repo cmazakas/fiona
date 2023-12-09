0467a2d4b606bd57ea11b38316b122737564b9f9
Christian Mazakas <christian.mazakas@gmail.com>
2023-12-09 09:09:26 -0800

    Add CMake add_subdirectory() test

---
+++ test/cmake_subdir_test/main.cpp
@@ -0,0 +1,18 @@
+#include <fiona/executor.hpp>
+#include <fiona/io_context.hpp>
+#include <fiona/task.hpp>
+#include <fiona/time.hpp>
+
+int
+main() {
+  fiona::io_context ioc;
+  fiona::timer timer( ioc.get_executor() );
+
+  ioc.post( []( fiona::timer timer ) -> fiona::task<void> {
+    auto mok = co_await timer.async_wait( std::chrono::milliseconds{ 250 } );
+    (void)mok.value();
+    co_return;
+  }( timer ) );
+
+  ioc.run();
+}
