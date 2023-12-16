23f9c9b404b177358981fa5f3d2b577a4691b7fd
Christian Mazakas <christian.mazakas@gmail.com>
2023-12-16 07:38:35 -0800

    Update waker_test to include multiple concurrent foreign futures

--- test/waker_test.cpp
+++ test/waker_test.cpp
@@ -106,3 +106,23 @@ TEST_CASE( "waker_test - waker outlives the io_context" ) {
   std::this_thread::sleep_for( std::chrono::milliseconds( 750 ) );
   CHECK( num_runs == 3 );
 }
+
+TEST_CASE( "waker_test - awaiting multiple foreign futures" ) {
+  num_runs = 0;
+
+  constexpr int const num_futures = 100;
+
+  fiona::io_context ioc;
+  for ( int i = 0; i < num_futures; ++i ) {
+    ioc.post( FIONA_TASK( fiona::executor ex ) {
+      duration_guard dg( std::chrono::milliseconds( 500 ) );
+      auto nums = co_await custom_awaitable( ex );
+      CHECK( nums == std::vector{ 1, 2, 3, 4 } );
+      ++num_runs;
+      co_return;
+    }( ioc.get_executor() ) );
+  }
+  ioc.run();
+
+  CHECK( num_runs == 2 * num_futures );
+}
