4b1fa022f2905aa41beca0761e808d1306a446e2
Christian Mazakas <christian.mazakas@gmail.com>
2024-02-10 07:31:00 -0800

    Separate TLS component from main CML export

--- cmake/FionaTest.cmake
+++ cmake/FionaTest.cmake
@@ -8,7 +8,20 @@ function(fiona_test testname)
   target_link_libraries(
     ${testname}
     PRIVATE
-      fiona
+      Fiona::fiona
+      Boost::headers
+      Catch2::Catch2 Catch2::Catch2WithMain
+  )
+  catch_discover_tests(${testname})
+endfunction()
+
+function(fiona_tls_test testname)
+  add_executable(${testname} helpers.hpp ${testname}.cpp)
+  target_link_libraries(
+    ${testname}
+    PRIVATE
+      Fiona::fiona
+      Fiona::tls
       Boost::headers
       Catch2::Catch2 Catch2::Catch2WithMain
   )
