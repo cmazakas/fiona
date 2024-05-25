8eb339be8b8b1dd95a14f27ae100249097b3853e
Christian Mazakas <christian.mazakas@gmail.com>
2024-05-25 08:26:04 -0700

    clean up CML and config macros for symbol import/export

--- include/fiona/detail/config.hpp
+++ include/fiona/detail/config.hpp
@@ -9,8 +9,16 @@
 #else
 #define FIONA_DECL BOOST_SYMBOL_IMPORT
 #endif
+
+#if defined( FIONA_TLS_SOURCE )
+#define FIONA_TLS_DECL BOOST_SYMBOL_EXPORT
+#else
+#define FIONA_TLS_DECL BOOST_SYMBOL_IMPORT
+#endif
+
 #else
 #define FIONA_DECL
+#define FIONA_TLS_DECL
 #endif
 
 #endif // FIONA_DETAIL_CONFIG_HPP
